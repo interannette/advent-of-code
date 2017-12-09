21ffaa416609afa5a0d6fa055ecc78c961a9ee9d
Annette Spyker <aspyker@gmail.com>
2017-12-09 12:46:57 -0600

    Part 2 day 5

--- day5.py
+++ day5.py
@@ -1,10 +1,23 @@
+part_two = True
+
+
+def transform_value(i):
+    if part_two:
+        if i >= 3:
+            return i - 1
+        else:
+            return i + 1
+    else:
+        return i + 1
+
+
 def count_steps(maze):
     l = len(maze)
     i = 0
     count = 0
     while i < l:
         step = maze[i]
-        maze[i] += 1
+        maze[i] = transform_value(maze[i])
         i += step
         count += 1
     return count
@@ -13,4 +26,3 @@ def count_steps(maze):
 def solve_maze(input_string):
     list_strings = input_string.split('\n')
     return count_steps(map(int, list_strings))
-

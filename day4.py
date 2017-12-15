c2fd0bd260726a593932b61bcfbce43221b5ea1d
Annette Spyker <aspyker@gmail.com>
2017-12-14 22:23:15 -0600

    Dat 4 part 2

--- day4.py
+++ day4.py
@@ -1,8 +1,24 @@
+part_two = True
+
+
+def word_is_duplicated(word, word_set):
+    if part_two:
+        word_letters = sorted(list(word))
+        for other_word in word_set:
+            if len(word) == len(other_word):
+                other_word_letters = list(other_word)
+                if word_letters == sorted(other_word_letters):
+                    return True
+        return False
+    else:
+        return word in word_set
+
+
 def passphrase_is_valid(passphrase):
     words = passphrase.split()
     word_set = set()
     for word in words:
-        if word in word_set:
+        if word_is_duplicated(word, word_set):
             return False
         else:
             word_set.add(word)

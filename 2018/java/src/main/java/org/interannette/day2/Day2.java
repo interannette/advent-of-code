3d0cb81c064adaf5fbd6d0c375fed98b56a836dd
Annette Spyker <aspyker@gmail.com>
2018-12-02 00:40:21 -0600

    Day 2 star 2

--- 2018/java/src/main/java/org/interannette/day2/Day2.java
+++ 2018/java/src/main/java/org/interannette/day2/Day2.java
@@ -1,5 +1,6 @@
 package org.interannette.day2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -7,16 +8,55 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Day2 {
-    public static String INPUT_STRING ="";
+
     public static void main(String args[]) {
         Day2 day2 = new Day2(INPUT_STRING);
         System.out.println("Checksum is: " + day2.solveStar1());
+        System.out.println("Matching characters are: " + day2.solveStar2());
     }
 
     private List<String> ids;
 
     public Day2(String idList) {
         this.ids = Arrays.asList(idList.split("\n"));
+    }
+
+    public String solveStar2() {
+        String s1 = null;
+        Integer differencePosition = null;
+
+        for(int i = 0; i < ids.size(); i++) {
+            for(int j = i+1; j < ids.size(); j++) {
+                List<Integer> differencePositions = differencePos(ids.get(i), ids.get(j));
+                if(differencePositions.size() == 1) {
+                    s1 = ids.get(i);
+                    differencePosition = differencePositions.get(0);
+                    break;
+                }
+            }
+        }
+
+        if(differencePosition == null || s1 == null) {
+            throw new RuntimeException("No match found");
+        }
+        return s1.substring(0, differencePosition) + s1.substring(differencePosition + 1);
+    }
+
+    protected static List<Integer> differencePos(String s1, String s2) {
+
+        if(s1.length() != s2.length()) {
+            throw new IllegalArgumentException("Strings should all be the same size");
+
+        }
+
+        List<Integer> differencePositions = new ArrayList<>(s1.length());
+        for(int i = 0; i < s1.length(); i++) {
+            if(s1.charAt(i) != s2.charAt(i)) {
+                differencePositions.add(i);
+            }
+        }
+
+        return differencePositions;
     }
 
     public long solveStar1() {
@@ -45,4 +85,6 @@ public class Day2 {
 
         return countCounts;
     }
+
+    private static String INPUT_STRING = "";
 }
--- 2018/java/src/test/java/org/interannette/day2/Day2Test.java
+++ 2018/java/src/test/java/org/interannette/day2/Day2Test.java
@@ -3,15 +3,46 @@ package org.interannette.day2;
 import lombok.Data;
 import org.interannette.StarTestCase;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Day2Test {
+
+    @Test
+    void differencePositionFinder() {
+        List<Integer> differences = Day2.differencePos("abcde", "axcye");
+        assertEquals(2, differences.size());
+        assertTrue(differences.contains(1));
+        assertTrue(differences.contains(3));
+    }
+
+    @ParameterizedTest
+    @MethodSource("star2TestCases")
+    void solveStar2(StarTestCase<String> testCase) {
+        Day2 testDay = new Day2(testCase.getInputString());
+        assertEquals(testCase.getExpectedOutput(), testDay.solveStar2());
+    }
+
+    static Stream<StarTestCase<String>> star2TestCases() {
+        return Stream.of(
+                new StarTestCase<>("abcde\n" +
+                        "fghij\n" +
+                        "klmno\n" +
+                        "pqrst\n" +
+                        "fguij\n" +
+                        "axcye\n" +
+                        "wvxyz",
+                        "fgij")
+        );
+    }
 
     @ParameterizedTest
     @MethodSource("star1TestCases")

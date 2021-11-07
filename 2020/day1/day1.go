9dc9a6e5d7f63f206cda8aeee509728de7658332
Annette Spyker <aspyker@gmail.com>
2021-11-07 12:59:00 -0600

    2020 day 1 star 2

--- 2020/day1/day1.go
+++ 2020/day1/day1.go
@@ -21,7 +21,7 @@ func getInput(sample bool) []int {
 
 	scanner := bufio.NewScanner(file)
 
-	a := make([]int, 100)
+	a := make([]int, 0)
 	for scanner.Scan() {
 		l := scanner.Text()
 		if i, err := strconv.Atoi(l); err == nil {
@@ -33,20 +33,37 @@ func getInput(sample bool) []int {
 	return a
 }
 
-func find(a []int) (int, int) {
+func find_2(a []int, t int) (int, int) {
 	for i := 0; i < len(a); i++ {
-		c := 2020 - a[i]
-		for j := 1; j < len(a); j++ {
+		c := t - a[i]
+		for j := (i + 1); j < len(a); j++ {
 			if a[j] == c {
 				return a[i], a[j]
 			}
 		}
 	}
-	return 0, 0
+
+	return -1, -1
+}
+
+func find_3(a []int, t int) (int, int, int) {
+	for i := 0; i < len(a); i++ {
+		c := t - a[i]
+		x, y := find_2(a[1:], c)
+		if x > 0 {
+			return a[i], x, y
+		}
+	}
+	return -1, -1, -1
 }
 
 func main() {
 	a := getInput(false)
-	i, j := find(a)
-	fmt.Printf("%d product. Components %d, %d", i*j, i, j)
+
+	i, j := find_2(a, 2020)
+	fmt.Printf("%d product. Components %d, %d\n", i*j, i, j)
+
+	i, j, k := find_3(a, 2020)
+	fmt.Printf("%d product. Components %d, %d, %d\n", i*j*k, i, j, k)
+
 }

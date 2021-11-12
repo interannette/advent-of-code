1a9ffa19841b6f771f3d625b283b236820387e69
Annette Spyker <aspyker@gmail.com>
2021-11-11 21:10:50 -0600

    2020 day 5 star 2

--- 2020/day05/day5.go
+++ 2020/day05/day5.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	// "regexp"
 )
 
 func getInput(sample bool) []string {
@@ -35,17 +34,21 @@ func getInput(sample bool) []string {
 	return input
 }
 
-func computeSeatIds(input []string) []int64 {
-	seats := make([]int64, 0)
-	for _, s := range input {
-		seats = append(seats, computeSeatId(s))
+func computeSeatIds(seats [][]int64) []int64 {
+	seatIds := make([]int64, 0)
+	for _, s := range seats {
+		seatIds = append(seatIds, s[0]*8+s[1])
 	}
-	return seats
+	return seatIds
 }
 
-func computeSeatId(s string) int64 {
-	r, c := computeSeat(s)
-	return r*8 + c
+func computeSeatList(input []string) [][]int64 {
+	seats := make([][]int64, 0)
+	for _, s := range input {
+		r, c := computeSeat(s)
+		seats = append(seats, []int64{r, c})
+	}
+	return seats
 }
 
 func computeSeat(s string) (row int64, col int64) {
@@ -80,10 +83,53 @@ func findMax(l []int64) int64 {
 	return max
 }
 
-func main() {
-	s := getInput(false)
-	ids := computeSeatIds(s)
-	max := findMax(ids)
+func findMissingSeat(seatMap [][]bool) (row int, col int) {
 
+	for i := 1; i < 127; i++ {
+		row := seatMap[i]
+		for j := 0; j < 8; j++ {
+			if !row[j] {
+				front := seatMap[i-1][j]
+				back := seatMap[i+1][j]
+				if front && back {
+					return i, j
+				}
+			}
+		}
+	}
+
+	return -1, -1
+}
+
+func buildSeatMap(seats [][]int64) [][]bool {
+	seatMap := make([][]bool, 128)
+	for i := 0; i <= 127; i++ {
+		seatMap[i] = make([]bool, 8)
+	}
+
+	for _, seat := range seats {
+		seatMap[seat[0]][seat[1]] = true
+	}
+
+	return seatMap
+}
+
+func printSeatMap(seatMap [][]bool) {
+	for i, row := range seatMap {
+		fmt.Printf("Row %d:\t", i)
+		fmt.Println(row)
+	}
+}
+
+func main() {
+	input := getInput(false)
+
+	seats := computeSeatList(input)
+	ids := computeSeatIds(seats)
+	max := findMax(ids)
 	fmt.Println(max)
+
+	seatMap := buildSeatMap(seats)
+	r, c := findMissingSeat(seatMap)
+	fmt.Printf("Row %d, col %d. Id %d\n", r, c, r*8+c)
 }

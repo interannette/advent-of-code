d1559fd2ccae206d7fcdfb4b7370b4f90300724d
Annette Spyker <aspyker@gmail.com>
2021-11-15 22:14:40 -0600

    2020 day 7 first star

--- 2020/day07/day7.go
+++ 2020/day07/day7.go
@@ -60,7 +60,7 @@ func parseRules(input []string) []rule {
 	return rules
 }
 
-var ruleRegex = regexp.MustCompile(`^(?P<color>[a-z0-9 ]+) bags contain (?P<condition>[a-z0-9 ]+).*`)
+var ruleRegex = regexp.MustCompile(`^(?P<color>[a-z0-9 ]+) bags contain (?P<condition>[a-z0-9 ,]+).$`)
 
 //input = append(input, "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.")
 //input = append(input, "faded blue bags contain no other bags.")
@@ -77,10 +77,17 @@ func parseRule(s string) rule {
 	conditions := make([]condition, 0)
 	if conidtionString != "no other bags" {
 		conditionSubstrings := strings.Split(conidtionString, ",")
+		if len(conditions) > 1 {
+			fmt.Println("Found rule with multiple conditions")
+		}
 		for _, c := range conditionSubstrings {
 			condition := parseCondition(c)
 			conditions = append(conditions, condition)
 		}
+	}
+
+	if len(conditions) > 1 {
+		fmt.Println("Found rule with multiple conditions")
 	}
 
 	r := rule{
@@ -111,8 +118,44 @@ func parseCondition(s string) condition {
 	return c
 }
 
+func buildMap(rules []rule) map[string][]string {
+
+	colorMap := make(map[string][]string)
+
+	for _, r := range rules {
+		//fmt.Println("Rule: ")
+		//fmt.Println(r)
+		for _, c := range r.conditions {
+			cur := colorMap[c.color]
+			cur = append(cur, r.color)
+			colorMap[c.color] = cur
+			//fmt.Println(colorMap[c.color])
+		}
+	}
+
+	return colorMap
+}
+
+func findContainingColors(colorMap map[string][]string, color string, containingColors map[string]bool) map[string]bool {
+
+	nextColors := colorMap[color]
+
+	if len(nextColors) == 0 {
+		return containingColors
+	}
+
+	for _, c := range nextColors {
+		containingColors[c] = true
+		containingColors = findContainingColors(colorMap, c, containingColors)
+	}
+
+	return containingColors
+}
+
 func main() {
-	input := getInput(true)
+	input := getInput(false)
 	rules := parseRules(input)
-	fmt.Println(rules)
+	colorsMap := buildMap(rules)
+	containerColors := findContainingColors(colorsMap, "shiny gold", make(map[string]bool))
+	fmt.Println(len(containerColors))
 }

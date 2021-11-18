774ae482f96aec5848757ff9d43e405e56f67b47
Annette Spyker <aspyker@gmail.com>
2021-11-17 22:27:54 -0600

    2020 day 8 part 2

--- 2020/day08/day8.go
+++ 2020/day08/day8.go
@@ -52,16 +52,16 @@ func parseInstruction(line string) instruction {
 	}
 }
 
-func runInstructions(program []instruction) int {
+func runInstructions(program []instruction) (int, bool) {
 	val := 0
 	i := 0
 
-	for {
+	for i < len(program) {
 		cur := program[i]
 		fmt.Printf("Executing %+v\n", cur)
 
 		if cur.visited == true {
-			return val
+			return val, true
 		}
 
 		program[i].visited = true
@@ -75,10 +75,57 @@ func runInstructions(program []instruction) int {
 			i = i + cur.argument
 		}
 	}
+
+	return val, false
+}
+
+func reset(program []instruction) {
+	for i := 0; i < len(program); i++ {
+		program[i].visited = false
+	}
+}
+
+func fixProgram(program []instruction) int {
+
+	for i := 0; i < len(program); i++ {
+		cur := program[i]
+		fmt.Printf("Testing line %d: %+v\n", i, cur)
+		if cur.operation != "acc" {
+
+			var newOp string
+			if cur.operation == "jmp" {
+				newOp = "nop"
+			} else {
+				newOp = "jmp"
+			}
+
+			update := instruction{
+				operation: newOp,
+				argument:  cur.argument,
+				visited:   false,
+			}
+			program[i] = update
+
+			val, loop := runInstructions(program)
+			if !loop {
+				return val
+			} else {
+				fmt.Println("Still loops")
+			}
+
+			program[i] = cur
+			reset(program)
+		}
+	}
+
+	return -1
 }
 
 func main() {
 	instructions := getInput("input.txt")
-	val := runInstructions(instructions)
-	fmt.Println(val)
+	val, loop := runInstructions(instructions)
+	fmt.Printf("Result: %d. Infinite loop: %v\n", val, loop)
+
+	val = fixProgram(instructions)
+	fmt.Printf("Fixed program returns %d", val)
 }

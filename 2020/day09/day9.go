8acd13aea5e70ab7dc1cdf9300cc6f13ab910651
Annette Spyker Mechelke <aspyker@gmail.com>
2021-11-18 20:20:57 -0600

    2020 day 9 part 2

--- 2020/day09/day9.go
+++ 2020/day09/day9.go
@@ -41,13 +41,54 @@ func isComposable(list []int, current int, lookback int) bool {
 	return false
 }
 
+func findContiguousSum(input []int, invalid int) (int, int) {
+	for i := 0; i < len(input); i++ {
+		sum := input[i]
+		for j := i + 1; j < len(input); j++ {
+			sum += input[j]
+			if sum == invalid {
+				return i, j
+			} else if sum > invalid {
+				break
+			}
+		}
+	}
+	return -1, -1
+}
+
+func findMinMax(input []int, start int, end int) (int, int) {
+	min := input[start]
+	max := input[start]
+	for i := start; i <= end; i++ {
+		if input[i] < min {
+			min = input[i]
+		}
+
+		if input[i] > max {
+			max = input[i]
+		}
+	}
+
+	return min, max
+}
+
 func main() {
 	input := getInput("input.txt")
 
 	preamble := 25
+	invalid := 0
 	for i := preamble; i < len(input); i++ {
 		if !isComposable(input, i, preamble) {
-			fmt.Println(input[i])
+			invalid = input[i]
+			break
 		}
 	}
+	fmt.Printf("Invalid number %d\n", invalid)
+
+	start, end := findContiguousSum(input, invalid)
+	fmt.Printf("Start %d (%d), end %d (%d).\n", start, input[start], end, input[end])
+
+	min, max := findMinMax(input, start, end)
+	fmt.Printf("Min %d, max %d. Sum %d\n", min, max, min+max)
+
 }

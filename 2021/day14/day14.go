af1359971639ef5de24cb81cd15bee975b10a417
Annette Spyker <aspyker@gmail.com>
2021-12-16 22:06:40 -0600

    2021 day 14 both stars

--- 2021/day14/day14.go
+++ 2021/day14/day14.go
@@ -43,13 +43,20 @@ func ParseRule(s string) rule {
 type input struct {
 	template []string
 	rules map[pair]string
+	polymerCounts map[pair]int
 }
 
 func NewInput(s string) input {
 	t := strings.Split(s, "")
+	counts := make(map[pair]int)
+	for i:=0; i < len(t) - 1; i++ {
+		p := NewPair(t[i], t[i+1])
+		counts[p] = counts[p] + 1
+	}
 	return input {
 		template: t,
 		rules: make(map[pair]string),
+		polymerCounts:counts,
 	}
 }
 
@@ -58,23 +65,35 @@ func (i *input) addRule(r rule) {
 }
 
 func (i *input) advance() {
-	newTemplate := make([]string,0)
-	for j := 0; j < len(i.template)-1; j++ {
-		
-		first := i.template[j]
-		second := i.template[j+1]
-		
-		newTemplate = append(newTemplate, first)
+	
+	newCounts := make(map[pair]int)
 
-		r, exists := i.rules[NewPair(first, second)]
+	for k, v := range i.polymerCounts {
+		
+		insert, exists := i.rules[k]
+
 		if exists {
-			newTemplate = append(newTemplate, r)
-		}
-		
-	}
-	newTemplate = append(newTemplate, i.template[len(i.template)-1])
 
-	i.template = newTemplate
+			p1 := NewPair(k.first, insert)
+			p2 := NewPair(insert, k.second)
+		
+			newCounts[p1] = newCounts[p1] + v
+			newCounts[p2] = newCounts[p2] + v
+
+		} else {
+			fmt.Printf("No matching rule for %v", k)
+		}
+	}
+
+	i.polymerCounts = newCounts
+}
+
+func (i input) lengthOfPolymer() int {
+	length := 1 // we don't have the last char in the map
+	for _, v := range i.polymerCounts {
+		length += v
+	}
+	return length
 }
 
 func getInput(fileName string) input {
@@ -96,23 +115,24 @@ func getInput(fileName string) input {
 }
 
 func main() {
-	file := "sample"
-	steps := 10
+	file := "input"
+	steps := 40
 
-	p := getInput(file + ".txt")
+	input := getInput(file + ".txt")
 
 	for i := 1; i <= steps; i++ {
-		p.advance()
-		fmt.Printf("Step %d. Template length %d\n", i, len(p.template))
+		input.advance()
 	}
 
 	countMap := make(map[string]int)
-	for _, s := range p.template {
-		countMap[s] = countMap[s] + 1
+	for p, c := range input.polymerCounts {
+		countMap[p.first] = countMap[p.first] + c
 	}
+	lastChar := input.template[len(input.template) - 1]
+	countMap[lastChar] = countMap[lastChar] + 1
 
 	maxCount := 0
-	minCount := len(p.template) + 1
+	minCount := input.lengthOfPolymer() + 1
 	for _, v := range countMap {
 		if v < minCount {
 			minCount = v
